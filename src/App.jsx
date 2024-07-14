2e3ba23e84578b1a2b5b39957de914f43d7e6476
Lucas Lucier <dev.lxcier@gmail.com>
2024-07-14 06:41:53 -0300

    atualizando código do 'app'

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 import axios from "axios";
 
-class App extends React.Component {
-    state = {
-       advice: "",
+const App = () => {
+    const [advice, setAdvice] = useState("");
+
+    const fetchAdvice = async () => {
+        try {
+            const response = await axios.get("https://api.adviceslip.com/advice");
+            const { advice } = response.data.slip;
+            setAdvice(advice);
+        } catch (error) {
+            console.error("Error fetching advice:", error);
+        }
     };
 
-    componentDidMount() {
-        this.fetchAdvice();
-    }
+    useEffect(() => {
+        fetchAdvice();
+    }, []);
 
-    fetchAdvice = () => {
-        axios
-            .get("https://api.adviceslip.com/advice")
-            .then((response) => {
-                const { advice } = response.data.slip;
-
-                this.setState({ advice });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
-
-    render() {
-        const { advice } = this.state;
-
-        return (
-            <div className="app">
-                <div className="card">
-                    <h1 className="heading">{advice}</h1>
-					<button className="button" onClick={this.fetchAdvice}>
-						<span>GIVE ME ADVICE!</span>
-					</button>
-				</div>
+    return (
+        <div className="app">
+            <div className="card">
+                <h2 className="heading">{advice}</h2>
+                <button className="button" onClick={fetchAdvice}>
+                    <span>GIVE ME ADVICE!</span>
+                </button>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default App;
